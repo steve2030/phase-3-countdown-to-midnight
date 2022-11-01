8de270c303ff119c66cb218b47d33c2a9255631c
steve <stevemighty2030@gmail.com>
2022-11-01 03:28:45 +0300

    complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(count_to_midnight)
+  while count_to_midnight > 0
+    puts "#{count_to_midnight} SECOND(S)!"
+    count_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep count_to_midnight
+  while count_to_midnight > 0
+    puts "#{count_to_midnight} SECOND(S)!"
+    sleep 1
+    count_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
